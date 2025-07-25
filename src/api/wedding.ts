3ec26a4952d6aa6ce2c370f4b6e5a7a67b0a3956
Yangsooyoung <64643668+Yang-Sooyoung@users.noreply.github.com>
2025-07-25 16:32:24 +0900

    Update wedding.ts

--- src/api/wedding.ts
+++ src/api/wedding.ts
@@ -1,3 +1,4 @@
 export function getWedding() {
-  return fetch('http://localhost:8888/wedding')
+  const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8888'
+  return fetch(`${BASE_URL}/wedding`)
 }
